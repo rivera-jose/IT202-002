5c4e8c9cde9596281eae57238af56a170d770b5a
jose-rivera <jpr47@njit.edu>
2022-02-25 21:29:17 -0500

    added home.php and logout.php and modified nav.php, login.php, and register.php

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,7 +1,6 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -11,8 +10,8 @@ require(__DIR__ . "/../../partials/nav.php");
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
-    <input type="submit" value="Register" />
-</?form>
+    <input type="submit" value="Login" />
+</form>
 <script>
     function validate(form) {
         //TODO 1: implement JavaScript validation
@@ -22,64 +21,57 @@ require(__DIR__ . "/../../partials/nav.php");
     }
 </script>
 <?php
- //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"])){
-     $email = se($_POST, "email", "", false);
-     $password = se($_POST, "password", "", false);
-     
-     //TODO 3: validate/use
-     $hasError=false;
-     if(empty($email))
-     {
-         echo "Email must not be empty";
-         $hasError=true;
-     }
-     //sanitize
-     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
 
-     //validate
-     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-         echo "Invalid email address";
-         $hasError=true;
-     }
-     if(empty($password))
-     {
-         echo "Password must not be empty";
-         $hasError=true;
-     }
-     if(strlen($password) < 8)
-     {
-         echo "password too short";
-         $hasError=true;
-     }
-
-     if(!$hasError)
-     {
-         echo "Welcome, $email";
-         //TODO 4
-         $db = getDB();
-         $stmt = $db->prepare("SELECT id, username, email, password from Users where email = :email");
-         try {
-             $r = $stmt->execute([":email" => $email]);
-             if($r)
-             {
-                 $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                 if($user){
-                     $hash = $user["password"];
-                     unset($user["password"]);
-                     if(password_verify($password, $hash)){
-                         echo "Welcome $email";
+    //TODO 3
+    $hasError = false;
+    if (empty($email)) {
+        echo "Email must not be empty";
+        $hasError = true;
+    }
+    //sanitize
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //validate
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address";
+        $hasError = true;
+    }
+    if (empty($password)) {
+        echo "password must not be empty";
+        $hasError = true;
+    }
+    if (strlen($password) < 8) {
+        echo "Password too short";
+        $hasError = true;
+    }
+    if (!$hasError) {
+        //TODO 4
+        $db = getDB();
+        $stmt = $db->prepare("SELECT id, email, password from Users where email = :email");
+        try {
+            $r = $stmt->execute([":email" => $email]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
+                    unset($user["password"]);
+                    if (password_verify($password, $hash)) {
+                        echo "Weclome $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid password";
-                    } 
+                    }
                 } else {
                     echo "Email not found";
                 }
             }
-        } catch(Exception $e)
-        {
+        } catch (Exception $e) {
             echo "<pre>" . var_export($e, true) . "</pre>";
         }
     }
-}  
+}
 ?>
