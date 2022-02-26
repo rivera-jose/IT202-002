f747e3aba2f430dcdf4216708765f37eb8b45e3b
jose-rivera <jpr47@njit.edu>
2022-02-25 20:13:27 -0500

    adding nav.php and login.php and modified register.php

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -11,10 +11,6 @@ require(__DIR__ . "/../../partials/nav.php");
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
-    <div>
-        <label for="confirm">Confirm</label>
-        <input type="password" name="confirm" required minlength="8" />
-    </div>
     <input type="submit" value="Register" />
 </?form>
 <script>
@@ -27,10 +23,9 @@ require(__DIR__ . "/../../partials/nav.php");
 </script>
 <?php
  //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])){
+ if(isset($_POST["email"]) && isset($_POST["password"])){
      $email = se($_POST, "email", "", false);
      $password = se($_POST, "password", "", false);
-     $confirm = se($_POST, "confirm", "", false);
      
      //TODO 3: validate/use
      $hasError=false;
@@ -52,42 +47,39 @@ require(__DIR__ . "/../../partials/nav.php");
          echo "Password must not be empty";
          $hasError=true;
      }
-     if(empty($confirm))
-     {
-         echo "Confirm password must not be empty";
-         $hasError=true;
-     }
      if(strlen($password) < 8)
      {
          echo "password too short";
-         $hasError=true;
-     }
-     if (
-         strlen($password) > 0 && $password !== $confirm
-     ) {
-         echo "passwords do not match";
          $hasError=true;
      }
 
      if(!$hasError)
      {
          echo "Welcome, $email";
-     
-     
-     //TODO 4
-     $hash = password_hash($password, PASSWORD_BCRYPT);
-     $db = getDB();
-     $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
-     try
-     {
-         $stmt->execute([":email" => $email, ":password" => $hash]);
-         echo "successfully registered!";
-     } catch (Exception $e) {
-         echo "there was a problem registering";
-         "<pre>" . var_export($e, true) . "</pre>";
+         //TODO 4
+         $db = getDB();
+         $stmt = $db->prepare("SELECT id, username, email, password from Users where email = :email");
+         try {
+             $r = $stmt->execute([":email" => $email]);
+             if($r)
+             {
+                 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                 if($user){
+                     $hash = $user["password"];
+                     unset($user["password"]);
+                     if(password_verify($password, $hash)){
+                         echo "Welcome $email";
+                    } else {
+                        echo "Invalid password";
+                    } 
+                } else {
+                    echo "Email not found";
+                }
+            }
+        } catch(Exception $e)
+        {
+            echo "<pre>" . var_export($e, true) . "</pre>";
         }
-    }  
-
-}
-
+    }
+}  
 ?>
