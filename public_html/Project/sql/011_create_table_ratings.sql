b6b720f8d970310fc383238d9fd296da2040cb47
jose-rivera <jpr47@njit.edu>
2022-05-05 21:45:11 -0400

    feat-productRatings

---
+++ public_html/Project/sql/011_create_table_ratings.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS Ratings(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    product_id int,
+    user_id int,
+    rating int,
+    comment text,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (product_id) REFERENCES Products(id)
+)
